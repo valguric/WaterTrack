864287c92dc1976203e9ded206403817fecae51a
Jood Areabi <areabijoodschl@gmail.com>
2023-10-05 17:21:32 +0300

    Initial Commit

---
+++ WaterTrack/WaterTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterTrackApp.swift
+//  WaterTrack
+//
+//  Created by jood on 05/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WaterTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
